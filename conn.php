60693046c9a0c7a8c092c388a869b49b9918a296
Robert James <xpindia@gmail.com>
2024-06-27 17:46:36 +0530

    conn.php file created

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php
+// Establish a connection to teh database.
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Connected successfully";
+        }
+        
+
+
+?>
