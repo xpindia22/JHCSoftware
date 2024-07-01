f0e587e089879cda570144dc2e91604e0c50ba72
Robert James <xpindia@gmail.com>
2024-07-01 14:59:50 +0530

    Session file created and edited

--- session_check.php
+++ session_check.php
@@ -4,7 +4,7 @@ session_start(); // Start the session
 // Check if the user is logged in (you can adjust this condition based on your authentication logic)
 if (!isset($_SESSION['id'])) {
     // Redirect to the login page or display an error message
-    header('Location: login.php'); // Example: Redirect to login.php
+    header('Location: login.html'); // Example: Redirect to login.php
     exit;
 }
 ?>
