0cbd4cdbdf2b7e9422fede65a505bda3079622bc
Robert James <xpindia@gmail.com>
2024-07-01 05:53:32 +0530

    Login branch created. Cleaned it of all main files in it. Created login,html, login.php, register.html, register.php. Created a database called mydb, created a table users. Inside the table created columns.

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "mydb"; // Use the database name you created
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = md5($_POST['password1']); // Hash the password
+
+// Insert user data into the 'users' table
+$sql = "INSERT INTO users (fname, lname, username, email, password) 
+        VALUES ('$fname', '$lname', '$username', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "User registered successfully";
+} else {
+    echo "Error registering user: " . $conn->error;
+}
+
+$conn->close();
+?>
