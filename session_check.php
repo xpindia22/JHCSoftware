1ddf99d6f56fe6447bf6830352fa1c9112677dd6
Robert James <xpindia@gmail.com>
2024-07-01 07:59:25 +0530

    session_check id changed.

--- session_check.php
+++ session_check.php
@@ -2,7 +2,7 @@
 session_start(); // Start the session
 
 // Check if the user is logged in (you can adjust this condition based on your authentication logic)
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['id'])) {
     // Redirect to the login page or display an error message
     header('Location: login.php'); // Example: Redirect to login.php
     exit;
