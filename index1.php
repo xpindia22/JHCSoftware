3d4029dbe69db63d24b7444ed77e50ebe8322d4a
Robert James <xpindia@gmail.com>
2024-07-03 10:58:16 +0530

    ccc

--- index1.php
+++ index1.php
@@ -1,6 +1,6 @@
 <!-- index.php -->
 <?php
-// require_once 'session_check.php'; // Include the session check
+require_once 'session_check.php'; // Include the session check
 echo "hello";
 // Your index.php code here
 ?>
