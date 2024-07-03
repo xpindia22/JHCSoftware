afdd5582c59b66c0d11f9971ef3368e11801a92f
Robert James <xpindia@gmail.com>
2024-07-03 12:27:21 +0530

    cc

--- 004_new-revisit.php
+++ 004_new-revisit.php
@@ -116,7 +116,7 @@ echo "<h2>Add Revisit Consultation for unit_no: $unit_no </h2>
       <td><input type='date' id='date' name='date'></td>
     </tr>
     <tr>
-      <td colspan='2'><input type='submit' name='add' value='Add Revisit'></td>
+      <td colspan='2'><input type='submit' name='add' value='Create Consultation Visit'></td>
     </tr>
 </form></table>";
 
