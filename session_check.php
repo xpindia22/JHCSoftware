a182f4d7e7c49def8cb2a5e336b9c1fc6ea65386
Robert James <xpindia@gmail.com>
2024-07-04 15:11:15 +0530

    cc

--- session_check.php
+++ session_check.php
@@ -6,5 +6,5 @@ if (!isset($_SESSION['userid'])) {
     exit;
 }
 $userid = $_SESSION['userid']; // retrieve the user ID from the session
-echo "<p><a href='logout.php'>You are logged in as $userid, Logout</a></p>";
+// echo "<p><a href='logout.php'>You are logged in as $userid, Logout</a></p>";
 ?>
