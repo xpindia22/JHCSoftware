2b3a6b4031b68c656c7efa01b3416acae67775a7
Robert James <xpindia@gmail.com>
2024-07-11 18:05:16 +0530

    Copied admin folder from admin branch

---
+++ admin/session_admin.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header("Location: admin_login.php");
+    exit();
+}
+
+$admin_username = $_SESSION['admin_username'];
+?>
