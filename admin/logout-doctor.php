7a1934c23b428975a444fe8444b4ac3630812739
Robert James <xpindia@gmail.com>
2024-07-12 11:07:50 +0530

    cc

--- admin/logout-doctor.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Location: 004_doctor_login.php');
-exit;
-?>
--- config/config.php
+++
@@ -1,2 +0,0 @@
-define('SESSION_CHECK_PATH', __DIR__ . '/config/session_check.php');
-define('CONN_PATH', __DIR__ . '/config/conn.php');
