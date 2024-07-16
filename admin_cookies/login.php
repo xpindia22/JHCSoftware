92383301f368fa5b598af38614569a3ad06491ef
Robert James <xpindia@gmail.com>
2024-07-16 21:46:05 +0530

    cc

--- admin_cookies/login.php
+++ admin_cookies/login.php
@@ -5,7 +5,7 @@ if(isset($_POST['submit'])){
     $password = $_POST['pass'];
 
 
-    $sql = "select * from login where username = '$username'and password = '$password'"; 
+    $sql = "select * from users where username = '$username'and password = '$password'"; 
     $result = mysqli_query($conn, $sql); 
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $count = mysqli_num_rows($result);  
@@ -20,3 +20,35 @@ if(isset($_POST['submit'])){
      }
 }
 ?>
+<?php
+  include("conn.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+    <div id="form">
+        <h1>Login Form</h1>
+        <form name="form"action="" method="POST">
+            <label>Username: </label>
+            <input type="text" id="user" name="user">
+            <br>
+            <br>
+            <label>Password</label>
+            <input type="password" id="pass" name="pass">
+<br>
+<br>
+
+            <input type="submit" id="btn" value="login" name="submit"/>
+</form>
+</div>
+
+</body>
+</html>
--- admin_cookies/login1.php
+++ admin_cookies/login1.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit'])) {
     $username = $_POST['user'];
     $password = $_POST['pass'];
 
-    $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $count = mysqli_num_rows($result);
