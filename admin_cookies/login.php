d00ee971091836f1bb9711282067554bedc668d5
Robert James <xpindia@gmail.com>
2024-07-17 08:23:05 +0530

    cc

--- admin_cookies/login.php
+++ admin_cookies/login.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
     
-    $sql = "SELECT userid, username, password, department, email, phone FROM users WHERE username = '$username'";
+    $sql = "SELECT userid, username, password, department, email, phone FROM admin WHERE username = '$username'";
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0) {
