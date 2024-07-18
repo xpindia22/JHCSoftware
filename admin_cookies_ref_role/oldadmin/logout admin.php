fcdfbf210bedfb13b8b34d9796d241e82818f095
Robert James <xpindia@gmail.com>
2024-07-18 22:56:26 +0530

    cc

--- admin_cookies_ref_role/oldadmin/logout admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Clear cookies
-setcookie('admin_id', '', time() - 3600, "/");
-setcookie('admin_username', '', time() - 3600, "/");
-
-// Redirect to login page
-header("Location: admin_login.php");
-exit();
-?>
