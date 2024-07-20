b92ada126a7b79bbe1d07a2d0730108cb09f08d0
Robert James <xpindia@gmail.com>
2024-07-20 19:54:59 +0530

    cc

--- logout-doctor.php
+++ logout-doctor.php
@@ -1,6 +1,39 @@
 <?php
+// session_start();
+
+// // Unset all session variables
+// $_SESSION = array();
+
+// // Destroy the session
+// session_destroy();
+
+// // Delete cookies by setting expiration time in the past
+// if (isset($_COOKIE['userid'])) {
+//     setcookie('userid', '', time() - 3600, '/');
+// }
+// if (isset($_COOKIE['username'])) {
+//     setcookie('username', '', time() - 3600, '/');
+// }
+
+// // Redirect to login page
+// header("Location: login.php");
+// exit();
+?>
+<?php
 session_start();
+
+// Unset all of the session variables
+$_SESSION = [];
+
+// Destroy the session.
 session_destroy();
-header('Location: 004_doctor_login.php');
+
+// Delete the cookies by setting their expiration date to a past date
+setcookie("userid", "", time() - 3600, "/");
+setcookie("username", "", time() - 3600, "/");
+setcookie("roles", "", time() - 3600, "/");
+
+// Redirect to the login page
+header("Location: login.php");
 exit;
 ?>
