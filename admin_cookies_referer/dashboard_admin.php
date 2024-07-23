9e620b16de2baac7ee30a3f765b0dba2ebb602a1
Robert James <xpindia@gmail.com>
2024-07-23 10:29:20 +0530

    Blacklist Updared

--- admin_cookies_referer/dashboard_admin.php
+++ admin_cookies_referer/dashboard_admin.php
@@ -21,4 +21,7 @@ echo "<ul>";
 ?>
 
 <p><a href='../admin_cookies_referer/staff_register.php'>Register Staff</a></p>
+<br>
+<p><a href='../admin_cookies_referer/blacklist.php'>Blacklist Staff</a></p>
+
 <p><a href='logout.php'>Logout</a></p>
